ddf065dffe1da549026fe2e4ed130d32b06f434a
Niymer <11106813+niymer@user.noreply.gitee.com>
2023-09-03 20:54:51 +0800

    提交未格式化的仓库

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="box">
+    <h1>我是App根目录文件</h1>
+  </div>
+</template>
+<script setup lang="ts"></script>
+
+<style scoped lang="scss">
+.box {
+  width: 600px;
+  height: 400px;
+  background-color: black;
+  h1 {
+    color: white;
+  }
+}
+</style>
