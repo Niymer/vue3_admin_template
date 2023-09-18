7d7a4b986e31c7a9348c4e830000575a44a581eb
Niymer <2328462082@qq.com>
2023-09-19 00:50:13 +0800

    fix: update

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,46 +1,48 @@
 <script setup lang="ts">
-import useUserStore from "@/store/modules/user.ts";
-import {getTime} from "../../utils/time.ts";
+import useUserStore from '@/store/modules/user.ts'
+import { getTime } from '../../utils/time.ts'
 
-let userStore=useUserStore()
-
+let userStore = useUserStore()
 </script>
 
 <template>
   <el-card>
     <div class="box">
-      <el-avatar :src="userStore.avatar" style="width: 100px;height: 100px;"></el-avatar>
+      <el-avatar
+        :src="userStore.avatar"
+        style="width: 100px; height: 100px"
+      ></el-avatar>
       <div class="bottom">
-        <h3 class="title">{{getTime()}}好!{{userStore.username}}</h3>
+        <h3 class="title">{{ getTime() }}好!{{ userStore.username }}</h3>
         <p class="subtitle">硅谷甄选运营平台</p>
       </div>
     </div>
   </el-card>
   <div class="bgc">
-    <svg-icon name="welcome" width="500px" height="500px"/>
+    <svg-icon name="welcome" width="500px" height="500px" />
   </div>
 </template>
 
 <style scoped lang="scss">
-.box{
+.box {
   display: flex;
-  .bottom{
+  .bottom {
     margin-left: 20px;
     display: flex;
     flex-direction: column;
     justify-content: center;
-    .title{
+    .title {
       font-size: 30px;
       font-weight: 900;
       margin-bottom: 30px;
     }
-    .subtitle{
+    .subtitle {
       font-style: italic;
       color: tan;
     }
   }
 }
-.bgc{
+.bgc {
   margin-top: 10px;
   display: flex;
   justify-content: center;
